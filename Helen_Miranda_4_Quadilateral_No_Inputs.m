1999e74b4d8bffbcb9d83513b221fac1ca21a602
CrazyScholar98 <omair.shafiq1998@gmail.com>
2023-06-15 14:45:42 +0500

    This is good

--- Helen_Miranda_4_Quadilateral_No_Inputs.m
+++ Helen_Miranda_4_Quadilateral_No_Inputs.m
@@ -12,7 +12,7 @@ tic %starts the clock
 
     % This part will calculate infered Essential variables
     % ------------------------------------------------------------------------
-    
+    %comit
     global geom ngpb nf_g  dees ngps deeb connec total_numbers_of_active_dof Number_of_Nodes Number_of_Elements Degrees_of_Freedom_Per_Element number_of_nodes_per_element nf dim number_of_dof_per_node
 
             number_of_dof_per_node = evalin('base','nodof');
