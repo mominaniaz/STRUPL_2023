89dbe8235c14b7cffbb041e576173cc4350f7f93
helenmiranda-30 <136704002+helenmiranda-30@users.noreply.github.com>
2023-07-27 16:44:45 +0200

    Function_elastic_solve
    it works correctly

--- STRUPL_01_06_23_A/TEST_STRUPL/Copy_of_CST_PLANE_STRESS.m
+++ STRUPL_01_06_23_A/TEST_STRUPL/Copy_of_CST_PLANE_STRESS.m
@@ -52,55 +52,57 @@ end
 %%%%%%%%%%%%%%%%%%%%%%% End of assembly %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %% Compute global displacement vector and reaction%%
-delta = KK\fg ; % solve for unknown displacements
-Reaction=KK*delta-fg;
-%
-node_disp=zeros(nnd,2);
-%
-for i=1: nnd %
-if nf(i,1) == 0 %
-x_disp =0.; %
-else
-x_disp = delta(nf(i,1)); %
-end
-%
-if nf(i,2) == 0 %
-y_disp = 0.; %
-else
-y_disp = delta(nf(i,2)); %
-end
-node_disp(i,:) =[x_disp y_disp];
-end
-%
-% Retrieve the x_coord and y_disp of the nodes located on the neutral axis
-%
-k = 0;
-for i=1:nnd;
-if geom(i,2)== 0.
-k=k+1;
-x_coord(k) = geom(i,1);
-vertical_disp(k)=node_disp(i,2);
-end
-end
-%% Compute stress and strain %%
-%
-for i=1:nel
-[bee,g,A] = elem_T3(i); % Form strain matrix, and steering vector
-eld=zeros(eldof,1); % Initialize element displacement to zero
-for m=1:eldof
-if g(m)==0 
-    eld(m)=0.;
-else %
-eld(m)=delta(g(m)); % Retrieve element displacement
-end
-end
-%
-eps=bee*eld; % Compute strains
-EPS(i,:)=eps ; % Store strains for all elements
-sigma=dee*eps; % Compute stresses
-SIGMA(i,:)=sigma ; % Store stress for all elements
-end
-%
+[delta,Reaction,node_displacement,SIGMA,STRAIN]=Elastic_solve(KK,fg,nf,dee,bee);
+
+% delta = KK\fg ; % solve for unknown displacements
+% Reaction=KK*delta-fg;
+% %
+% node_disp=zeros(nnd,2);
+% %
+% for i=1: nnd %
+% if nf(i,1) == 0 %
+% x_disp =0.; %
+% else
+% x_disp = delta(nf(i,1)); %
+% end
+% %
+% if nf(i,2) == 0 %
+% y_disp = 0.; %
+% else
+% y_disp = delta(nf(i,2)); %
+% end
+% node_disp(i,:) =[x_disp y_disp];
+% end
+% %
+% % Retrieve the x_coord and y_disp of the nodes located on the neutral axis
+% %
+% k = 0;
+% for i=1:nnd;
+% if geom(i,2)== 0.
+% k=k+1;
+% x_coord(k) = geom(i,1);
+% vertical_disp(k)=node_disp(i,2);
+% end
+% end
+% %% Compute stress and strain %%
+% %
+% for i=1:nel
+% [bee,g,A] = elem_T3(i); % Form strain matrix, and steering vector
+% eld=zeros(eldof,1); % Initialize element displacement to zero
+% for m=1:eldof
+% if g(m)==0 
+%     eld(m)=0.;
+% else %
+% eld(m)=delta(g(m)); % Retrieve element displacement
+% end
+% end
+% %
+% eps=bee*eld; % Compute strains
+% EPS(i,:)=eps ; % Store strains for all elements
+% sigma=dee*eps; % Compute stresses
+% SIGMA(i,:)=sigma ; % Store stress for all elements
+% end
+% %
 %% Compute for each element the plastic multiplier that activates the plastic mode This part is a function related to yield function drucker pragher [alpha_b,Normal_stress]
 %Macro-cracks
 sigma_t = 0.1198; % Limit tension [MPa]
