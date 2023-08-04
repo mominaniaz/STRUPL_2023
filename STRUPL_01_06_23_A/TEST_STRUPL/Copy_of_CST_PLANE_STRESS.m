30b3790bfa1c3f35bd0511218a339245463fbdd2
helenmiranda-30 <136704002+helenmiranda-30@users.noreply.github.com>
2023-08-04 11:41:27 +0200

    Average stress recovery
    this part intends to compute the stress at each node. Doesn't work at the moment

--- STRUPL_01_06_23_A/TEST_STRUPL/Copy_of_CST_PLANE_STRESS.m
+++ STRUPL_01_06_23_A/TEST_STRUPL/Copy_of_CST_PLANE_STRESS.m
@@ -53,60 +53,25 @@ end
 %
 %% Compute global displacement vector and reaction%%
 [delta,Reaction,node_displacement,SIGMA,STRAIN]=Elastic_solve(KK,fg,nf,dee,bee,thick);
-% for i=1:nel
-% Internal_forces=zeros(nnd,1);  
-% element_force=[] ;
-% Internal_forces=Internal_forces+bee'*sigma*A*thick; 
-% element_force=[element_force; Internal_forces'];
-% end
-% delta = KK\fg ; % solve for unknown displacements
-% Reaction=KK*delta-fg;
-% %
-% node_disp=zeros(nnd,2);
-% %
-% for i=1: nnd %
-% if nf(i,1) == 0 %
-% x_disp =0.; %
-% else
-% x_disp = delta(nf(i,1)); %
-% end
-% %
-% if nf(i,2) == 0 %
-% y_disp = 0.; %
-% else
-% y_disp = delta(nf(i,2)); %
-% end
-% node_disp(i,:) =[x_disp y_disp];
-% end
-% %
-% % Retrieve the x_coord and y_disp of the nodes located on the neutral axis
-% %
-% k = 0;
-% for i=1:nnd;
-% if geom(i,2)== 0.
-% k=k+1;
-% x_coord(k) = geom(i,1);
-% vertical_disp(k)=node_disp(i,2);
-% end
-% end
-% %% Compute stress and strain %%
-% %
-% for i=1:nel
-% [bee,g,A] = elem_T3(i); % Form strain matrix, and steering vector
-% eld=zeros(eldof,1); % Initialize element displacement to zero
-% for m=1:eldof
-% if g(m)==0 
-%     eld(m)=0.;
-% else %
-% eld(m)=delta(g(m)); % Retrieve element displacement
-% end
-% end
-% %
-% eps=bee*eld; % Compute strains
-% EPS(i,:)=eps ; % Store strains for all elements
-% sigma=dee*eps; % Compute stresses
-% SIGMA(i,:)=sigma ; % Store stress for all elements
-% end
+%
+type='average';
+type='sum';
+stressavg = zeros(nnd,3);
+for ind = 1:nnd
+    [r c] = find(connec==ind) ;
+    switch type
+        case 'average'
+        share = length(r) ;
+        case 'sum'
+        share = 1 ;
+    end
+    ele = 4*(r-1)+c ;
+    stress=[sum(stress_N(ele,1))/share.....
+            sum(stress_N(ele,2))/share.....
+            sum(stress_N(ele,3))/share];                        
+    stressavg(ind,:)=stress;
+end
+
 % %
 %% Compute for each element the plastic multiplier that activates the plastic mode This part is a function related to yield function drucker pragher [alpha_b,Normal_stress]
 %Macro-cracks parameters
@@ -333,7 +298,7 @@ Vector_b_plastic = Vector_b_plastic.*Volume;
 
 
 
-[A_Matrix]=form_Matrix_A(Normality_Matrix,dee,bee,KK);
+% [A_Matrix]=form_Matrix_A(Normality_Matrix,dee,bee,KK);
 % Print results to file
 %
 print_CST_results;
@@ -345,7 +310,7 @@ cmin = min(x_stress);
 cmax = max(x_stress);
 clim([cmin cmax])
 patch('Faces', connec, 'Vertices', geom, 'FaceVertexCData',x_stress, ...
-'Facecolor','flat','Marker','o')
+'Facecolor','flat','Marker','.')
 colorbar;
 
 toc
