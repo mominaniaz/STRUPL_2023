83639b26890191c0cd21501ad306e706294e5454
mominaniaz <135269780+mominaniaz@users.noreply.github.com>
2023-12-14 23:34:53 +0500

    Classes

---
+++ Classes/Structure.m
@@ -0,0 +1,14 @@
+    classdef Structure
+        properties
+            type_of_element = []
+            thickness of plate = 0; %thickness of the structure
+            Elastic_modulus = 0; % elastic modulus of the material
+            dimension = 0;
+            poissons_ratio = 0; 
+            Number_of_degree_of_freedom_per_node = 0;
+            fm = 0; % compressive strenght of mortar
+            ft =0; % tensile strength of mortar
+
+
+        end
+    end
